5e0052c9ac3ea006216f92c50da29a789a396c5d
HngDng <vidang68@gmail.com>
2018-11-13 23:54:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hngdng/ass/database/DatabaseHelper.java
@@ -0,0 +1,24 @@
+package com.example.hngdng.ass.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    public DatabaseHelper(@androidx.annotation.Nullable @Nullable Context context, @androidx.annotation.Nullable @Nullable String name, @androidx.annotation.Nullable @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
